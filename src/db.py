f429f2c8a99fa108fb400d28d5d50a5d5d62e102
Ming Yi <myi@student.ethz.ch>
2020-04-16 22:35:24 +0200

    add query for year bounds, and all genres

--- src/db.py
+++ src/db.py
@@ -1,10 +1,12 @@
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, ASCENDING, DESCENDING
 from app import app
 
 # configure database
 db_url = 'localhost:27017'
 app.config['MONGO_URI'] = 'mongodb://{}/ase'.format(db_url)
-db = PyMongo(app).db
+pymongo = PyMongo(app)
+db = pymongo.db
+
 
 def card_query(game_id, round_num):
     fields = {'_id': 0, 'id': 1, 'revenue': 1, 'runtime': 1, 'popularity': 1, 'title': 1}
@@ -12,3 +14,50 @@ def card_query(game_id, round_num):
     cursor = db.movies.find(filter = condition, projection = fields, skip = (round_num - 1) * 20).limit(20)
     docs = list(cursor)
     return docs
+
+
+def year_bounds():
+    """
+    find the lower bound and upper bound  of the year of movies
+    """
+    bounds = list(db.movies.aggregate([
+        { "$group": { 
+            "_id": None,
+            "lb": { "$max": "$release_date" }, 
+            "up": { "$min": "$release_date" } 
+        }},
+    ]))
+    lb = bounds[0]['lb']
+    up = bounds[0]['up']
+
+    # select the year from the string, or integer
+    if not isinstance(lb, int):
+        lb = int(lb[0:4])
+    if not isinstance(up, int):
+        up = int(up[0:4])
+    return lb, up
+
+
+def genre_all():
+    """
+    find distinct genres in the movies dataset
+    """
+    genres = db.movies.aggregate([
+        {
+            '$unwind': '$genres' # produce a new docs for each genre in the array
+        },
+        {
+          '$group':{
+              '_id': '$genres.name'
+          }  
+        }
+    ])
+    genre_list = [elem['_id'] for elem in list(genres)]
+    return genre_list
+
+
+# testing
+if __name__ == "__main__":
+    # print(card_query(1,1))
+    # print(year_bounds())
+    print(genre_all())
