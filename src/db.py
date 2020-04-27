ecdf41ced0c3db31527cc27fcb795e64003a6cdf
Ming Yi <myi@student.ethz.ch>
2020-04-27 16:27:17 +0200

    reutrn 20 movies and change rest api

--- src/db.py
+++ src/db.py
@@ -8,12 +8,12 @@ pymongo = PyMongo(app)
 db = pymongo.db
 
 
-def card_query(game_id, round_num):
+def card_query(round_num):
     fields = {'_id': 0, 'genres': 0, 'release_date': 0, 'movieId':0, 'id':0}
     # condition = {'revenue': {'$gt': 0}} # skip those movies with missing revenue
-    cursor = db.movies.find(projection = fields, skip = (round_num - 1) * 20).limit(1)
+    cursor = db.movies.find(projection = fields, skip = (round_num - 1) * 20).limit(20)
     docs = list(cursor)
-    print(docs[0].keys())
+    # print(docs[0].keys())
     return docs
 
 
